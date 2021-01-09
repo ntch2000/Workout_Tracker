6e4929087eabe991cad073b1c6e3b2e794c7c1d2
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-01-09 17:06:03 -0500

    adjusted comments in Workout.js schema file

--- models/Workout.js
+++ models/Workout.js
@@ -2,42 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
-  {
-    // create schema here
-    day: { type: Date, default: Date.now() },
+const WorkoutSchema = new Schema({
+  // Workout Schema
+  day: { type: Date, default: Date.now() },
 
-    // might need to change to exercises
-    exercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-        },
-        name: {
-          type: String,
-          trim: true,
-        },
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number,
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
       },
-    ],
-  },
-  {
-    toJSON: { virtuals: true },
-  }
-);
-
-// create virtual for totalDuration
-// WorkoutSchema.virtual("totalDuration").get(function () {
-//   //let totalDuration = 0;
-//   return this.exercises.reduce(function (total, exercises) {
-//     return parseInt(exercises.duration);
-//   });
-// });
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number,
+    },
+  ],
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
